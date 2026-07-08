e6110de3779c1231e7f88fcd14209c2dedc1eba2
Dean Hill <inkandflowers@proton.me>
2026-07-08 19:13:23 +0800

    Add style and naming notes

--- src/CQS_O_NaN.py
+++ src/CQS_O_NaN.py
@@ -1,10 +1,45 @@
 import numpy as np
 
-nInst=51
-currentPos = np.zeros(nInst)
-def getMyPosition (prcSoFar):
-    global currentPos
-    (nins,nt) = prcSoFar.shape
+"""
+NOTE: Variable Name Prefixes - Dean
+
+This can help a lot sometimes:
+    g_... : Denotes a global variable
+    m_... : Denotes a member variable of a class
+    c_... : Denotes a constant
+
+
+NOTE: Style - Dean
+
+I am currently keeping with the camel case variable
+naming of the original code (e.g. fizzBizz) but with
+underscores as I personally find they help 
+(e.g. fizz_Buzz).
+
+Putting spaces between parenthesises and arguments (as
+seen below) can help a lot. It's that they encouraged in
+the UNIX and C unit at the least 
+
+
+NOTE: Notes and ToDo's
+
+You may notice I have repeatedly used "NOTE:" in these
+comments. The use of "NOTE:" and "TODO:" is common 
+practice in code comments to denotes important notes 
+and things that need to be done. Many IDEs have functions
+to automatically search for these tags so they can be very
+useful.
+
+
+Please do let everyone know if you have any comments.
+"""
+
+g_Number_Of_Instruments = 51
+current_Position = np.zeros( number_Of_Instruments )
+
+def getMyPosition( prcSoFar ):
+    global current_Position
+    ( nins,nt ) = prcSoFar.shape
     if (nt < 2):
         return np.zeros(nins)
     lastRet = np.log(prcSoFar[:,-1] / prcSoFar[:,-2])
@@ -12,4 +47,4 @@ def getMyPosition (prcSoFar):
     lastRet /= lNorm
     rpos = np.array([int(x) for x in 5000 * lastRet / prcSoFar[:,-1]])
     currentPos = np.array([int(x) for x in currentPos+rpos])
-    return currentPos
+    return currentPoisiton 
