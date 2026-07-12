a773e9b45196adb46727c123aee506f6ca69f71f
Dean Hill <inkandflowers@proton.me>
2026-07-12 14:25:02 +0800

    Implement function to calculate the market mean at a given day

--- src/CQS_O_NaN.py
+++ src/CQS_O_NaN.py
@@ -339,6 +339,13 @@ def getAssetMARelativeArithmeticDrift( prices_So_Far, asset_Idx, desired_Latest_
 
     return ma_Relative_Arithmetic_Drift
 
+# Maret Average $
+def getMarketMean( prices_So_Far, day_Num ):
+    ( number_Of_Instruments, number_Of_Timesteps ) = prices_So_Far.shape
+    day_Num = min( day_Num, number_Of_Timesteps - 1 )
+    market_Mean = np.mean( prices_So_Far[ : ][ day_Num ] )
+
+    return market_Mean
 
 
 ##### Strategies #####
