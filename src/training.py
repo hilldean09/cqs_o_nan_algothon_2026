ae033c470fdd004882c9fc17db67542d164f1ed3
Dean Hill <inkandflowers@proton.me>
2026-07-22 17:51:13 +0800

    Continue neural net training environment

--- src/training.py
+++ src/training.py
@@ -18,6 +18,9 @@ class Training_Environment():
         self.timestep_Idx = 0
         self.previous_Position = np.zeros( self.number_Of_Instruments )
 
+        self.cash = 0
+        self.value = 0
+
     def _setTimestepPricesSoFar( self, timestep_Idx ):
         self.prices_So_Far = self.prices_Values[ : , : timesteps_Idx + 1 ]
 
@@ -31,21 +34,43 @@ class Training_Environment():
 
 
     # TODO: Write implementation
-    def _calculateReward( self, new_Position ):
-        pass
+    def _calculateStepReward( self, new_Position ):
+        # TODO: Add position limit clipping
+        current_Prices = self.prices_So_Far[ -1 ]
+        delta_Pos = new_Position - self.previous_Position
+
+        position_Value = new_Position.dot( current_Prices )
+
+        self.cash -= current_Prices.dot( delta_Pos )
+
+        today_PnL = cash + position_Value - self.value
+        self.value = self.cash + position_Value
+
+        return today_PnL
+
 
     def step( self, logits ):
         # returns a state vector corresponding to 
         # the neural net inputs
-        self._setTimestepPricesSoFar( self.timestep_Idx )
         return_Position = onan.runNeuralNetMasterStrategy( self.prices_So_Far, logits )
 
-        reward = self._calculateReward
+        reward = self._calculateReward( return_Position )
 
+        self.previous_Position = return_Position
         self.timestep += 1
 
-    # TODO: Write implementation
+        if( self.timestep_Idx < self.episode_Size ):
+            self._setTimestepPricesSoFar( self.timestep_Idx )
+            output_State = getNeuralNetInputs( self.prices_So_Far, self.timestep_Idx )
+            output_End = False
+        else:
+            output_End = True
+
+        return output_State, reward, output_End
+
+
     def reset( self ):
-        pass
+        self._setInitialConditions()
+        self._setTimestepPricesSoFar( self.timestep_Idx )
 
 
